2bed050ef7b6c246623cd6a54d177f8b9083d759
chin-chillatw <chinr.yang@gmail.com>
2022-12-03 15:22:26 +0800

    sum.js更新

--- sum.js
+++ sum.js
@@ -1,12 +1,15 @@
-let total=0
+
 function sum(n){
+    let total=0;
  for (i=1; i<=n; i++){
+    
     total+=i
- }
- return(total)
+   
+ };
  
+ return(total)
 };
-console.log(sum(10));
+console.log(sum(5));
 console.log(sum(2));
 
 
