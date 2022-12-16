ad28fb45c7522ddd46859b88d45d74ab210bf174
chin-chillatw <chinr.yang@gmail.com>
2022-12-16 23:16:23 +0800

    crawler-await版 但跑不出來

---
+++ crawler/crawler-await.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+
+(async()=>{
+    try{
+    let stockNo = 2330
+    let date = '20221216'
+
+    let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
+        params:{ //其餘參數 parameters
+            stockNo,
+            date,
+        }
+    })
+    console.log(response.data)
+}catch(e){
+    console.error(e) 
+    }
+})
